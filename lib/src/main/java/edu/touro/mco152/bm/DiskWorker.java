035977cf6f58b219f768f1a2a0d60707b3a3842f
Aharon Seidman <aharondov1@gmail.com>
2023-03-20 15:16:43 -0400

    removed process and done methods from diskworker to SwingUIWorker.java diskworker edition

--- lib/src/main/java/edu/touro/mco152/bm/DiskWorker.java
+++ lib/src/main/java/edu/touro/mco152/bm/DiskWorker.java
@@ -40,8 +40,7 @@ import static edu.touro.mco152.bm.DiskMark.MarkType.WRITE;
 
 public class DiskWorker {
 
-    // Record any success or failure status returned from SwingWorker (might be us or super)
-    Boolean lastStatus = null;  // so far unknown
+
     private UIWorker uiWorker;
     public DiskWorker(UIWorker uiWorker){
         this.uiWorker = uiWorker;
@@ -293,41 +292,7 @@ public class DiskWorker {
         return true;
     }
 
-    /**
-     * Process a list of 'chunks' that have been processed, ie that our thread has previously
-     * published to Swing. For my info, watch Professor Cohen's video -
-     * Module_6_RefactorBadBM Swing_DiskWorker_Tutorial.mp4
-     * @param markList a list of DiskMark objects reflecting some completed benchmarks
-     */
-    @Override
-    protected void process(List<DiskMark> markList) {
-        markList.stream().forEach((dm) -> {
-            if (dm.type == DiskMark.MarkType.WRITE) {
-                Gui.addWriteMark(dm);
-            } else {
-                Gui.addReadMark(dm);
-            }
-        });
-    }
 
 
-    @Override
-    protected void done() {
-        // Obtain final status, might from doInBackground ret value, or SwingWorker error
-        try {
-            lastStatus = super.get();   // record for future access
-        } catch (Exception e) {
-            Logger.getLogger(App.class.getName()).warning("Problem obtaining final status: " + e.getMessage());
-        }
 
-        if (App.autoRemoveData) {
-            Util.deleteDirectory(dataDir);
-        }
-        App.state = App.State.IDLE_STATE;
-        Gui.mainFrame.adjustSensitivity();
-    }
-
-    public Boolean getLastStatus() {
-        return lastStatus;
-    }
 }
